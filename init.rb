5c0a92dd83af810f324b25eb387fa7f07e6308bb
michael <michael@iconoclast.net>
2010-10-12 22:01:22 -0700

    initial generated skeleton

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_document_folders do
+  name 'Redmine Document Folders plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
